517051af05bcc41df2a444094b8b0d4e9b50df87
BossEmma <128752098+BossEmma@users.noreply.github.com>
2024-05-27 05:29:26 -0700

    Update settings.py

--- WIG_backend/settings.py
+++ WIG_backend/settings.py
@@ -149,6 +149,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS=[os.path.join(BASE_DIR , "static")]
+
+STATIC_ROOT= BASE_DIR/"staticfiles"
+
+STATICFILES_STORAGE= "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
