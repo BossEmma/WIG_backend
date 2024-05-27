a82e749a2e7f72386a66cca7537fed60f023d0c2
BossEmma <128752098+BossEmma@users.noreply.github.com>
2024-05-27 13:16:09 +0100

    Add files via upload

---
+++ api/serializers.py
@@ -0,0 +1,110 @@
+from rest_framework import serializers
+from .models import User, Project, ProjectGallery
+from django.contrib.auth import authenticate
+
+
+
+class UserRegisterationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'first_name', 'last_name', 'phone_number', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+    
+    
+    
+ 
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
+    
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'phone_number', 'role', 'profile_pic']
+
+
+
+class UserProjectSerializer(serializers.ModelSerializer):
+    user = UserProfileSerializer(read_only=True) 
+    class Meta:
+        model = Project
+        fields = ('id', 'user', 'title', 'sub_header', 'content', 'date')
+
+
+
+class UserProjectImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectGallery
+        fields = ['image']
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'first_name', 'last_name', 'phone_number', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            phone_number=validated_data['phone_number'],
+            role=validated_data['role']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+"""
---
+++ api/views.py
@@ -0,0 +1,90 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.generics import GenericAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from . import serializers
+from .models import User, Project
+
+
+
+
+class UserRegisterationAPIView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = serializers.UserRegisterationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        data = serializer.data 
+        data["tokens"] = {"refresh": str(token), "access": str(token.access_token)}
+        return Response(data, status=status.HTTP_201_CREATED)
+
+
+
+class UserLoginAPIView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = serializers.UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token = RefreshToken.for_user(user)
+        data = serializer.data
+        data["tokens"] = {"refresh": str(token), "access": str(token.access_token)}
+        return Response(data, status=status.HTTP_200_OK)
+    
+
+
+class UserProfileView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.UserProfileSerializer
+
+    def get_object(self):
+        return self.request.user
+    
+
+class UserProjectView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        user= self.request.user
+        projects = Project.objects.all().filter(user=request.user)
+        serializer = serializers.UserProjectSerializer(projects, many=True)
+        return Response(serializer.data)
+    
+
+class ProjectView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.UserProjectSerializer
+
+    def get_queryset(self):
+        return Project.objects.filter(user=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        title = self.kwargs.get('title')
+        try:
+            instance = self.get_queryset().get(title=title)
+        except Project.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+
+class UserLogoutAPIView(GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
